a38b4ed83b68e7418ed1005891b9c1e50a7625f1
Fábio Vinícius Lima <limavfabio@gmail.com>
2022-09-17 12:09:56 -0300

    Add Routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './Header.css';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header>
       <h1>todos</h1>
+      <NavLink>
+        <a className="about">about</a>
+      </NavLink>
     </header>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import TodoContainer from './components/TodoContainer';
 import './App.scss';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TodoContainer />
+    <BrowserRouter>
+      <TodoContainer />
+    </BrowserRouter>
   </React.StrictMode>
 );
